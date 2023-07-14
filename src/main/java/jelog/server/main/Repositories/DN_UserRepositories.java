0b8ec88d2034e75942baaf592ec45f42652e0536
jaeyoung9 <workerst@naver.com>
2023-07-14 17:56:55 +0900

    User Table nd Service Repositories

--- src/main/java/jelog/server/main/Repositories/DN_UserRepositories.java
+++ src/main/java/jelog/server/main/Repositories/DN_UserRepositories.java
@@ -2,8 +2,15 @@ package jelog.server.main.Repositories;
 
 
 import jelog.server.main.Model.DN_UserModel;
+import org.springframework.data.jpa.repository.EntityGraph;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+import java.util.List;
 
 
+@Repository
+public interface DN_UserRepositories extends JpaRepository<DN_UserModel, Integer> {
 
-public interface DN_UserRepositories {
+    List<DN_UserModel> findByDN_UID(Integer DN_UID);
 }
---
+++ src/main/java/jelog/server/main/Service/DN_UserService.java
@@ -0,0 +1,56 @@
+package jelog.server.main.Service;
+
+
+import jelog.server.main.Model.DN_UserModel;
+import jelog.server.main.Repositories.DN_UserRepositories;
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+/**
+ * [Service]
+ * DN_Users
+ * */
+//-----------------------------------------------------------------------------------------------------------------------------------------
+@Slf4j
+@Service
+public class DN_UserService {
+
+    /**
+     * [Variables]
+     * Repository
+     * */
+    //-------------------------------------------------------------------------------------------------------------------------------------
+    @Autowired
+    private DN_UserRepositories dn_userRepositories;
+
+    /**
+     * [User]
+     * Validate
+     * */
+    //-------------------------------------------------------------------------------------------------------------------------------------
+    public void validateUser(final DN_UserModel _model){
+        if(null == _model){
+            log.warn("data is check for cannot be null.");
+            throw new RuntimeException("data is check for cannot be null.");
+        }
+    }
+
+    /**
+     * [User]
+     * Add
+     * */
+    //-------------------------------------------------------------------------------------------------------------------------------------
+    public DN_UserModel createUser(final DN_UserModel entity){
+
+        // Data Check
+        validateUser(entity);
+
+        dn_userRepositories.save(entity);
+        log.info("Use : {} is saved.", entity.getDN_UID());
+        return dn_userRepositories.findById(entity.getDN_UID()).get();
+    }
+
+
+
+}
