bb2c0efed457a82b0737bbacd4d7073008e23be2
jaeyoung9 <workerst@naver.com>
2023-10-26 13:24:27 +0900

    editor.js - update

--- src/main/resources/static/js/editor.js
+++ src/main/resources/static/js/editor.js
@@ -1,27 +1,36 @@
 document.addEventListener('DOMContentLoaded', function () {
     const contentTitle = document.getElementById('title-input');
-    const contentBody = document.getElementById('text-area');
     const imageUploadInput = document.getElementById('image-upload-input');
     const selectEl = document.getElementById('selectEl');
     const textField = document.querySelector('div[name="textField"]');
     const buttons = document.querySelectorAll('div[name="formatting-buttons"] button');
+    const imageUploadButton = document.getElementById('image-upload-button');
+    const submitButton = document.getElementById('submit-button');
+
     let showCode = false;
     let contentThumbnail = '';
     textField.contentEditable = "true";
 
-    // Define DT_Files class to match the structure you want
+    const buttonCommands = {
+        createLink: 'createLink',
+        showCode: 'showCode',
+    };
+
+    function executeCommand(cmd, value = null) {
+        document.execCommand(cmd, false, value);
+    }
+
     class DT_Files {
         constructor() {
             this.fileId = 0;
             this.filePath = '';
             this.fileName = '';
             this.mediaType = '';
-            this.resultFile = []; // Initialize resultFile as an empty array
-            this.content = null; // Initialize content as null
+            this.resultFile = [];
+            this.content = null;
         }
     }
 
-    // Define DT_Content class to match the structure you want
     class DT_Content {
         constructor() {
             this.contentId = 0;
@@ -35,11 +44,11 @@ document.addEventListener('DOMContentLoaded', function () {
         }
     }
 
-    const content = new DT_Content(); // Create a new instance of DT_Content
-    const newFiles = []; // Initialize an array to store new files
+    const content = new DT_Content();
+    const newFiles = [];
 
-    // Add an event listener to the "insertImage" button
-    const imageUploadButton = document.getElementById('image-upload-button');
+
+
     imageUploadButton.addEventListener('click', () => {
         imageUploadInput.click();
     });
@@ -47,23 +56,22 @@ document.addEventListener('DOMContentLoaded', function () {
     imageUploadInput.addEventListener('change', (e) => {
         const file = e.target.files[0];
         if (file) {
-            // Handle the uploaded image here
+
             const reader = new FileReader();
             reader.onload = (event) => {
-                // Convert the ArrayBuffer to a Base64 data URL
+
                 const arrayBufferView = new Uint8Array(event.target.result);
                 const blob = new Blob([arrayBufferView], { type: file.type });
                 const imageUrl = URL.createObjectURL(blob);
 
-                // Create an image element and set its source
+
                 const img = document.createElement('img');
                 img.src = imageUrl;
                 img.alt = 'Image';
 
-                // Insert the image into the content
                 insertImageElement(img);
 
-                // Store the file in the newFiles array
+
                 newFiles.push(file);
             };
             reader.readAsArrayBuffer(file);
@@ -81,14 +89,14 @@ document.addEventListener('DOMContentLoaded', function () {
             const cmd = button.getAttribute('data-cmd');
 
             switch (cmd) {
-                case 'createLink':
+                case buttonCommands.createLink:
                     const url = prompt('Enter Link URL:', "https://");
                     if (url) {
                         executeCommand(cmd, url);
                     }
                     break;
 
-                case 'showCode':
+                case buttonCommands.showCode:
                     toggleCodeView();
                     break;
 
@@ -100,10 +108,6 @@ document.addEventListener('DOMContentLoaded', function () {
             }
         });
     });
-
-    function executeCommand(cmd, value = null) {
-        document.execCommand(cmd, false, value);
-    }
 
     function insertHTML(html) {
         document.execCommand('insertHTML', false, html);
@@ -127,7 +131,7 @@ document.addEventListener('DOMContentLoaded', function () {
         return supportedCommands.includes(cmd);
     }
 
-    const submitButton = document.getElementById('submit-button');
+
 
     submitButton.addEventListener('click', function () {
         content.contentCategories = selectEl.value;
@@ -136,7 +140,7 @@ document.addEventListener('DOMContentLoaded', function () {
         content.contentThumbnail = contentThumbnail;
         content.views = 0;
 
-        // Create an array to store promises for file conversions
+
         const fileConversionPromises = newFiles.map(file => {
             return new Promise((resolve, reject) => {
                 const dtFile = new DT_Files();
@@ -146,7 +150,7 @@ document.addEventListener('DOMContentLoaded', function () {
 
                 const reader = new FileReader();
                 reader.onload = function () {
-                    // Convert the ArrayBuffer to a byte array
+
                     const arrayBuffer = reader.result;
                     const byteArray = new Uint8Array(arrayBuffer);
                     dtFile.resultFile = Array.from(byteArray);
@@ -157,12 +161,12 @@ document.addEventListener('DOMContentLoaded', function () {
             });
         });
 
-        // Wait for all file conversions to complete
+
         Promise.all(fileConversionPromises)
             .then(dtFiles => {
                 content.files = dtFiles;
 
-                // Send the content object as JSON to the server
+
                 return goFetch('/api/auth/cwo/action', {
                     method: 'POST',
                     headers: {
@@ -173,12 +177,13 @@ document.addEventListener('DOMContentLoaded', function () {
             })
             .then(response => response.json())
             .then(data => {
-                // Handle the response data if needed
+
                 console.log(data);
             })
             .catch(error => {
-                // Handle any errors here
+
                 console.error(error);
             });
     });
+
 });
