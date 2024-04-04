ad313ecf2d39a4f478669c721875985700cd9f07
jaeyoung9 <workerst@naver.com>
2024-04-04 21:10:46 +0900

    Fix - Administrator function development scheduled.

---
+++ src/main/java/jelog/server/main/Controller/ADM1011/ADM1011Controller.java
@@ -0,0 +1,17 @@
+package jelog.server.main.Controller.ADM1011;
+
+/**
+ * Description :
+ * PackageName : jelog.server.main.Controller.ADM1011
+ * FileName : ADM1011Controller
+ * Author : User
+ * TimeDate : 2024-04-04
+ * ============================================================
+ * DATE                      AUTHOR                      NOTE
+ * ------------------------------------------------------------
+ * 2024-04-04               User                최초생성
+ * ------------------------------------------------------------
+ */
+public class ADM1011Controller {
+
+}
